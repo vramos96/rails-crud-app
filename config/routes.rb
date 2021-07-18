e9049e360948f06f9e91cc0b080bd4d8d6acbe34
Victor Ramos <victor@cuatropuntocero.solutions>
2021-07-18 11:50:14 -0700

    Agregado dismiss a alerts, agregada validación para sucursal antes de registro, removido caché

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'sucursals#index'
   post '/verificarEmpresa', to: 'admin#verificarEmpresa', as: 'verificar_empresa'
+  post '/verificarSucursal', to: 'admin#verificarSucursal', as: 'verificar_sucursal'
 end
